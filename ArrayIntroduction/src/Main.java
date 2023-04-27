a058eba8f330290a0997ea216a13cd65feca7a6c
Sofia Nascimento <sofia.n06@aluno.ifsc.edu.br>
2023-04-26 21:25:55 -0300

    Exercício Array

--- ArrayIntroduction/src/Main.java
+++ ArrayIntroduction/src/Main.java
@@ -4,10 +4,17 @@ public class Main {
        int[] myArray = new int[12];
        String[] strings = new String[10];
 
-       System.out.println(myArray[0]);
-       System.out.println(strings[0]);
-       System.out.println(myArray[0]);
-       System.out.println(myArray[11]);
-       System.out.println(myArray[5]);
+       myArray[0] = 5;
+       myArray[1] = 9;
+       myArray[2] = 4;
+
+       System.out.println(myArray[1]);
+
+       strings[0] = "Sofia";
+       strings[1] = "Ana";
+
+       System.out.println(strings[1]);
+
+
     }
 }
